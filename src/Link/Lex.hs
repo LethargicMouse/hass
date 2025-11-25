06925e4edcdd5f3c31fc479765a7161a6f0f2055
George Kozirev <gkozirev.0@gmail.com>
2025-11-25 14:20:48 +0300

    +lex skip spaces

--- src/Link/Lex.hs
+++ src/Link/Lex.hs
@@ -6,6 +6,7 @@ module Link.Lex where
 import Code (Code (..))
 import Data.ByteString.Char8 (isPrefixOf, unpack)
 import qualified Data.ByteString.Char8 as BS
+import Data.Char (isSpace)
 import Data.Vector (fromList)
 import Effectful (Eff)
 import Location (Location (..), poses)
@@ -14,11 +15,14 @@ import Shorts (Dies, die, enclosed)
 lex :: (Dies es) => Code -> Eff es [Token]
 lex (Code n s) = lex' <*> poses . unpack $ s
  where
-  lex' a ps
+  lex' a' ps'
     | BS.null a = pure [Token Eof loc]
     | has "fn" = (Token Fn loc :) <$> lex' (BS.drop 2 a) (drop 2 ps)
     | otherwise = die (lexError loc)
    where
+    spaces = BS.length $ BS.takeWhile isSpace a'
+    a = BS.drop spaces a'
+    ps = drop spaces ps'
     loc = Location n (head ps) ls
     has s' = s' `isPrefixOf` a
   ls = fromList (BS.lines s)
